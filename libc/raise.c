2e0b68da3e652e9ecddaddfa2001b16c18f402f6
Charles E. Youse <charles@gnuless.org>
2022-08-13 09:00:36 -0400

    libc/raise.c: add raise()
    this is just a silly wrapper for kill(),
    but it's required by POSIX and ANSI/ISO.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- libc/raise.c
+++ libc/raise.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-   signal.h                                      tahoe/64 standard library
+   raise.c                                       tahoe/64 standard library
 
 ******************************************************************************
 
@@ -31,52 +31,12 @@
 
 *****************************************************************************/
 
-#ifndef _SIGNAL_H
-#define _SIGNAL_H
+#include <unistd.h>
+#include <signal.h>
 
-#include <sys/tahoe.h>
-
-#ifndef __PID_T
-#define __PID_T
-typedef __pid_t pid_t;
-#endif /* __PID_T */
-
-#define SIGINT      2
-#define SIGILL      4
-#define SIGABRT     6
-#define SIGFPE      8
-#define SIGSEGV     11
-#define SIGTERM     15
-
-extern int kill(pid_t, int);
-extern int raise(int sig);
-
-typedef void(*__sighandler_t)(int);
-
-#define SIG_DFL ((__sighandler_t) 0)
-#define SIG_ERR ((__sighandler_t) -1)
-#define SIG_IGN ((__sighandler_t) 1)
-
-typedef unsigned long sigset_t;
-
-struct sigaction
+int raise(int sig)
 {
-    __sighandler_t sa_handler;
-    unsigned long sa_flags;
-    void (*sa_restorer)(void);              /* Linux ABI */
-    sigset_t sa_mask;
-};
-
-#define SA_RESETHAND    0x80000000
-#define SA_NODEFER      0x40000000
-#define SA_RESTORER     0x04000000          /* Linux ABI */
-
-extern __sighandler_t signal(int, __sighandler_t);
-extern void __sigreturn(void);
-extern int __sigaction(int, const struct sigaction *, struct sigaction *);
-extern int sigaction(int, const struct sigaction *, struct sigaction *);
-extern int sigemptyset(sigset_t *);
-
-#endif /* _SIGNAL_H */
+    kill(getpid(), sig);
+}
 
 /* vi: set ts=4 expandtab: */

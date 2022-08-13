f7cc9231ace09ed9e522838ed9c5ef5c277b8508
Charles E. Youse <charles@gnuless.org>
2022-08-13 11:51:38 -0400

    libc: add calloc()
    another legacy wrapper function preserved by ANSI and POSIX.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- libc/calloc.c
+++ libc/calloc.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-   stdlib.h                                      tahoe/64 standard library
+   calloc.c                                      tahoe/64 standard library
 
 ******************************************************************************
 
@@ -31,61 +31,19 @@
 
 *****************************************************************************/
 
-#ifndef _STDLIB_H
-#define _STDLIB_H
+#include <stdlib.h>
+#include <string.h>
 
-#include <sys/tahoe.h>
+void *calloc(size_t nmemb, size_t size)
+{
+    size_t n;
+    void *p;
 
-#define NULL        __NULL
+    n = nmemb * size;
+    p = malloc(n);
+    if (p) memset(p, 0, n);
 
-#ifndef __SIZE_T
-#define __SIZE_T
-typedef __size_t size_t;
-#endif /* __SIZE_T */
-
-#define WEXITSTATUS     __WEXITSTATUS
-#define WIFEXITED       __WIFEXITED
-#define WIFSTOPPED      __WIFSTOPPED
-#define WIFSIGNALED     __WIFSIGNALED
-#define WSTOPSIG        __WSTOPSIG
-#define WTERMSIG        __WTERMSIG
-
-extern void (*__exit_cleanup)(void);
-extern void __stdio_cleanup(void);
-
-extern int atoi(const char *);
-extern long atol(const char *);
-
-extern void *bsearch(const void *, const void *, size_t, size_t,
-                     int (*)(const void *, const void *));
-
-/* _exit() is the system call; exit() ties up the
-   library's loose ends before calling _exit(). */
-
-#define EXIT_SUCCESS    0
-#define EXIT_FAILURE    1
-
-extern void abort(void);
-extern void _exit(int);
-extern void exit(int);
-
-extern void *calloc(size_t, size_t);
-extern void free(void *);
-extern char *getenv(const char *);
-extern void *malloc(size_t);
-extern float strtof(const char *, char **);
-extern double strtod(const char *, char **);
-extern long strtol(const char *, char **, int);
-extern unsigned long strtoul(const char *, char **, int);
-
-extern void qsort(void *, size_t, size_t,
-                  int (*compar)(const void *, const void *));
-
-#define RAND_MAX    32767
-
-extern int rand(void);
-extern void srand(unsigned);
-
-#endif /* _STDLIB_H */
+    return p;
+}
 
 /* vi: set ts=4 expandtab: */

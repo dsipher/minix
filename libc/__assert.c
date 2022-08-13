b3f1961e5cff21cc192e12007a7438f4cc446c24
Charles E. Youse <charles@gnuless.org>
2022-08-13 11:30:56 -0400

    libc: add assert(), assert.h etc.
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

---
+++ libc/__assert.c
@@ -0,0 +1,46 @@
+/*****************************************************************************
+
+   __assert.c                                    tahoe/64 standard library
+
+******************************************************************************
+
+   Copyright (c) 2021, 2022, Charles E. Youse (charles@gnuless.org).
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS  PROVIDED BY  THE COPYRIGHT  HOLDERS AND  CONTRIBUTORS
+   "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES,  INCLUDING, BUT  NOT
+   LIMITED TO, THE  IMPLIED  WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS
+   FOR  A  PARTICULAR  PURPOSE  ARE  DISCLAIMED.  IN  NO  EVENT  SHALL THE
+   COPYRIGHT  HOLDER OR  CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL,  SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL  DAMAGES (INCLUDING,
+   BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+   OF USE, DATA, OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY  OF LIABILITY, WHETHER IN CONTRACT,  STRICT LIABILITY, OR
+   TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT OF THE
+   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <assert.h>
+#include <signal.h>
+
+void __assert(const char *e, const char *file, int line)
+{
+    fprintf(stderr, "assertion `%s' failed (in %s at line %d)\n", e,
+                                                                  file,
+                                                                  line);
+    abort();
+}
+
+/* vi: set ts=4 expandtab: */

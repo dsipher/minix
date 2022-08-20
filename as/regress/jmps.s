6e10baf029ed6a619d0e4b7e8fc58634a10a3522
Charles E. Youse <charles@gnuless.org>
2022-08-20 10:30:43 -0400

    as/regress: add conditional jumps to regression
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- as/regress/jmps.s
+++ as/regress/jmps.s
@@ -6,9 +6,72 @@
 		call %ax
 		call *%ax
 		call *%ebx
+		jo routine16
+		jno routine16
+		jb routine16
+		jc routine16
+		jnae routine16
+		jnb routine16
+		jnc routine16
+		jae routine16
+		jz routine16
+		je routine16
+		jnz routine16
+		jne routine16
+		jbe routine16
+		jna routine16
+		jnbe routine16
+		ja routine16
+		js routine16
+		jns routine16
+		jp routine16
+		jpe routine16
+		jnp routine16
+		jpo routine16
+		jl routine16
+		jnge routine16
+		jnl routine16
+		jge routine16
+		jle routine16
+		jng routine16
+		jnle routine16
+		jg routine16
+
+		.org 0xB0
+
 routine16:
 
 .code32
+		jo routine16
+		jno routine16
+		jb routine16
+		jc routine16
+		jnae routine16
+		jnb routine16
+		jnc routine16
+		jae routine16
+		jz routine16
+		je routine16
+		jnz routine16
+		jne routine16
+		jbe routine16
+		jna routine16
+		jnbe routine16
+		ja routine16
+		js routine16
+		jns routine16
+		jp routine16
+		jpe routine16
+		jnp routine16
+		jpo routine16
+		jl routine16
+		jnge routine16
+		jnl routine16
+		jge routine16
+		jle routine16
+		jng routine16
+		jnle routine16
+		jg routine16
 
 		call routine16
 		call routine32
@@ -25,4 +88,34 @@ routine32:
 		call %ax
 		call *%ax
 		call *%rbx
+		jo routine16
+		jno routine16
+		jb routine16
+		jc routine16
+		jnae routine16
+		jnb routine16
+		jnc routine16
+		jae routine16
+		jz routine16
+		je routine16
+		jnz routine16
+		jne routine16
+		jbe routine16
+		jna routine16
+		jnbe routine16
+		ja routine16
+		js routine16
+		jns routine16
+		jp routine16
+		jpe routine16
+		jnp routine16
+		jpo routine16
+		jl routine16
+		jnge routine16
+		jnl routine16
+		jge routine16
+		jle routine16
+		jng routine16
+		jnle routine16
+		jg routine16
 routine64:

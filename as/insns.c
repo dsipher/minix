b972ed6ce62f66c01a8310bb5e803e24a2de414c
Charles E. Youse <charles@gnuless.org>
2022-08-20 09:34:31 -0400

    as/insns.c: alphabetize by mnemonic
    no functional change. just keeping it organized.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- as/insns.c
+++ as/insns.c
@@ -34,7 +34,6 @@
 #include "as.h"
 
 struct insn i_aaa[]     =   { { I_NO_CODE64, 1, { 0x37 } }, { 0 } };
-struct insn i_aas[]     =   { { I_NO_CODE64, 1, { 0x3F } }, { 0 } };
 
 struct insn i_aad[] =
 {
@@ -50,230 +49,7 @@ struct insn i_aam[] =
     { 0 }
 };
 
-struct insn i_clc[]     =   { { 0, 1, { 0xF8 } }, { 0 } };
-struct insn i_cld[]     =   { { 0, 1, { 0xFC } }, { 0 } };
-struct insn i_cli[]     =   { { 0, 1, { 0xFA } }, { 0 } };
-struct insn i_cmc[]     =   { { 0, 1, { 0xF5 } }, { 0 } };
-struct insn i_hlt[]     =   { { 0, 1, { 0xF4 } }, { 0 } };
-struct insn i_lock[]    =   { { 0, 1, { 0xF0 } }, { 0 } };
-struct insn i_rep[]     =   { { 0, 1, { 0xF3 } }, { 0 } };
-struct insn i_ret[]     =   { { 0, 1, { 0xC3 } }, { 0 } };
-struct insn i_stc[]     =   { { 0, 1, { 0xF9 } }, { 0 } };
-struct insn i_std[]     =   { { 0, 1, { 0xFD } }, { 0 } };
-struct insn i_sti[]     =   { { 0, 1, { 0xFB } }, { 0 } };
-struct insn i_xlat[]    =   { { 0, 1, { 0xD7 } }, { 0 } };
-
-struct insn i_bsfw[] =
-{
-    { I_DATA_16, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_16, F_MID           },
-                                            { O_GPR_16 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_bsfl[] =
-{
-    { I_DATA_32, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_32, F_MID           },
-                                            { O_GPR_32 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_bsfq[] =
-{
-    { I_DATA_64, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_64, F_MID           },
-                                            { O_GPR_64 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_bsrw[] =
-{
-    { I_DATA_16, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_16, F_MID           },
-                                            { O_GPR_16 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_bsrl[] =
-{
-    { I_DATA_32, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_32, F_MID           },
-                                            { O_GPR_32 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_bsrq[] =
-{
-    { I_DATA_64, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_64, F_MID           },
-                                            { O_GPR_64 | O_MEM, F_MODRM } } },
-    { 0 }
-};
-
-struct insn i_seg[] =
-{
-    { 0, 1, { 0x26 }, { { O_SEG_2, F_MID } } },
-    { 0, 1, { 0x60 }, { { O_SEG_3, F_END } } },
-    { 0 }
-};
-
-struct insn i_iret[]  =
-{
-    { I_NO_CODE64, 1, { 0xCF } },       /* no REX if not in long mode */
-    { I_DATA_64,   1, { 0xCF } },       /* but always a REX in long mode */
-    { 0 }
-};
-
-struct insn i_call[] =
-{
-    { I_NO_CODE32 | I_NO_CODE64, 1, { 0xE8 }, { { O_REL_16 } } },
-    { I_NO_CODE16,               1, { 0xE8 }, { { O_REL_32 } } },
-
-    { I_DATA_16,     2, { 0xFF, 0x10 }, { { O_GPR_16, F_MODRM } } },
-
-    { I_DATA_32   |
-      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_GPR_32, F_MODRM } } },
-
-    /* assume code addresses in memory match the code size.
-       we do not provide separate call insns (via suffixes)
-       for other possibilities, even when they're legal. */
-
-    { I_DATA_16   |
-      I_NO_CODE32 |
-      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_MEM, F_MODRM } } },
-
-    { I_DATA_32   |
-      I_NO_CODE16 |
-      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_MEM, F_MODRM } } },
-
-    { I_DATA_64 |
-      I_NO_REX,      2, { 0xFF, 0x10 }, { { O_GPR_64 | O_MEM, F_MODRM } } },
-
-    { 0 }
-};
-
-struct insn i_jo[] =
-{
-    { 0,                            1, { 0x70       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x80 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x80 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jno[] =
-{
-    { 0,                            1, { 0x71       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x81 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x81 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jb[] =
-{
-    { 0,                            1, { 0x72       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x82 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x82 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jnb[] =
-{
-    { 0,                            1, { 0x73       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x83 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x83 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_je[] =
-{
-    { 0,                            1, { 0x74       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x84 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x84 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jne[] =
-{
-    { 0,                            1, { 0x75       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x85 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x85 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jbe[] =
-{
-    { 0,                            1, { 0x76       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x86 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x86 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jnbe[] =
-{
-    { 0,                            1, { 0x77       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x87 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x87 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_js[] =
-{
-    { 0,                            1, { 0x78       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x88 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x88 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jns[] =
-{
-    { 0,                            1, { 0x79       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x89 }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x89 }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jp[] =
-{
-    { 0,                            1, { 0x7A       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8A }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8A }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jnp[] =
-{
-    { 0,                            1, { 0x7B       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8B }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8B }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jl[] =
-{
-    { 0,                            1, { 0x7C       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8C }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8C }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jnl[] =
-{
-    { 0,                            1, { 0x7D       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8D }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8D }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jle[] =
-{
-    { 0,                            1, { 0x7E       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8E }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8E }, { { O_REL_16 } } },
-    { 0 }
-};
-
-struct insn i_jnle[] =
-{
-    { 0,                            1, { 0x7F       }, { { O_REL_8  } } },
-    { I_NO_CODE16,                  2, { 0x0F, 0x8F }, { { O_REL_32 } } },
-    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8F }, { { O_REL_16 } } },
-    { 0 }
-};
+struct insn i_aas[]     =   { { I_NO_CODE64, 1, { 0x3F } }, { 0 } };
 
 struct insn i_adcb[] =
 {
@@ -505,5 +281,232 @@ struct insn i_andq[] =
 
     { 0 }
 };
+
+struct insn i_bsfw[] =
+{
+    { I_DATA_16, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_16, F_MID           },
+                                            { O_GPR_16 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_bsfl[] =
+{
+    { I_DATA_32, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_32, F_MID           },
+                                            { O_GPR_32 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_bsfq[] =
+{
+    { I_DATA_64, 3, { 0x0F, 0xBC, 0x00 }, { { O_GPR_64, F_MID           },
+                                            { O_GPR_64 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_bsrw[] =
+{
+    { I_DATA_16, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_16, F_MID           },
+                                            { O_GPR_16 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_bsrl[] =
+{
+    { I_DATA_32, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_32, F_MID           },
+                                            { O_GPR_32 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_bsrq[] =
+{
+    { I_DATA_64, 3, { 0x0F, 0xBD, 0x00 }, { { O_GPR_64, F_MID           },
+                                            { O_GPR_64 | O_MEM, F_MODRM } } },
+    { 0 }
+};
+
+struct insn i_call[] =
+{
+    { I_NO_CODE32 | I_NO_CODE64, 1, { 0xE8 }, { { O_REL_16 } } },
+    { I_NO_CODE16,               1, { 0xE8 }, { { O_REL_32 } } },
+
+    { I_DATA_16,     2, { 0xFF, 0x10 }, { { O_GPR_16, F_MODRM } } },
+
+    { I_DATA_32   |
+      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_GPR_32, F_MODRM } } },
+
+    /* assume code addresses in memory match the code size.
+       we do not provide separate call insns (via suffixes)
+       for other possibilities, even when they're legal. */
+
+    { I_DATA_16   |
+      I_NO_CODE32 |
+      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_MEM, F_MODRM } } },
+
+    { I_DATA_32   |
+      I_NO_CODE16 |
+      I_NO_CODE64,   2, { 0xFF, 0x10 }, { { O_MEM, F_MODRM } } },
+
+    { I_DATA_64 |
+      I_NO_REX,      2, { 0xFF, 0x10 }, { { O_GPR_64 | O_MEM, F_MODRM } } },
+
+    { 0 }
+};
+
+struct insn i_clc[]     =   { { 0, 1, { 0xF8 } }, { 0 } };
+struct insn i_cld[]     =   { { 0, 1, { 0xFC } }, { 0 } };
+struct insn i_cli[]     =   { { 0, 1, { 0xFA } }, { 0 } };
+struct insn i_cmc[]     =   { { 0, 1, { 0xF5 } }, { 0 } };
+struct insn i_hlt[]     =   { { 0, 1, { 0xF4 } }, { 0 } };
+
+struct insn i_iret[]  =
+{
+    { I_NO_CODE64, 1, { 0xCF } },       /* no REX if not in long mode */
+    { I_DATA_64,   1, { 0xCF } },       /* but always a REX in long mode */
+    { 0 }
+};
+
+struct insn i_jb[] =
+{
+    { 0,                            1, { 0x72       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x82 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x82 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jbe[] =
+{
+    { 0,                            1, { 0x76       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x86 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x86 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_je[] =
+{
+    { 0,                            1, { 0x74       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x84 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x84 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jl[] =
+{
+    { 0,                            1, { 0x7C       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8C }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8C }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jle[] =
+{
+    { 0,                            1, { 0x7E       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8E }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8E }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jnb[] =
+{
+    { 0,                            1, { 0x73       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x83 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x83 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jnbe[] =
+{
+    { 0,                            1, { 0x77       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x87 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x87 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jne[] =
+{
+    { 0,                            1, { 0x75       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x85 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x85 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jnl[] =
+{
+    { 0,                            1, { 0x7D       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8D }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8D }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jnle[] =
+{
+    { 0,                            1, { 0x7F       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8F }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8F }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jno[] =
+{
+    { 0,                            1, { 0x71       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x81 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x81 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jnp[] =
+{
+    { 0,                            1, { 0x7B       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8B }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8B }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jns[] =
+{
+    { 0,                            1, { 0x79       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x89 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x89 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jo[] =
+{
+    { 0,                            1, { 0x70       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x80 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x80 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_jp[] =
+{
+    { 0,                            1, { 0x7A       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x8A }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x8A }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_js[] =
+{
+    { 0,                            1, { 0x78       }, { { O_REL_8  } } },
+    { I_NO_CODE16,                  2, { 0x0F, 0x88 }, { { O_REL_32 } } },
+    { I_NO_CODE32 | I_NO_CODE64,    2, { 0x0F, 0x88 }, { { O_REL_16 } } },
+    { 0 }
+};
+
+struct insn i_lock[]    =   { { 0, 1, { 0xF0 } }, { 0 } };
+struct insn i_rep[]     =   { { 0, 1, { 0xF3 } }, { 0 } };
+struct insn i_ret[]     =   { { 0, 1, { 0xC3 } }, { 0 } };
+
+struct insn i_seg[] =
+{
+    { 0, 1, { 0x26 }, { { O_SEG_2, F_MID } } },
+    { 0, 1, { 0x60 }, { { O_SEG_3, F_END } } },
+    { 0 }
+};
+
+struct insn i_stc[]     =   { { 0, 1, { 0xF9 } }, { 0 } };
+struct insn i_std[]     =   { { 0, 1, { 0xFD } }, { 0 } };
+struct insn i_sti[]     =   { { 0, 1, { 0xFB } }, { 0 } };
+struct insn i_xlat[]    =   { { 0, 1, { 0xD7 } }, { 0 } };
 
 /* vi: set ts=4 expandtab: */

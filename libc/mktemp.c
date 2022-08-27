f03b11168fbc41598be44a04f2c6bbeecd5d921e
Charles E. Youse <charles@gnuless.org>
2022-08-27 14:56:41 -0400

    libc/mktemp.c: replace mktemp() function
    completely rewritten from scratch. the COHERENT-based version
    had a few shortcomings: most problematic was its assumption
    that process IDs could be represented in five decimal digits.
    now we use a base-62 encoding for the process ID and sequence
    digit. it's also now more robust and in agreement with POSIX.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- libc/mktemp.c
+++ libc/mktemp.c
@@ -5,22 +5,17 @@
 ******************************************************************************
 
    Copyright (c) 2021, 2022, Charles E. Youse (charles@gnuless.org).
-   derived from COHERENT, Copyright (c) 1977-1995 by Robert Swartz.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
 
-   1. Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
 
-   2. Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-   3. Neither the name of the copyright holder nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
 
    THIS SOFTWARE IS  PROVIDED BY  THE COPYRIGHT  HOLDERS AND  CONTRIBUTORS
    "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES,  INCLUDING, BUT  NOT
@@ -37,57 +32,75 @@
 *****************************************************************************/
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 
 /* produce a unique filename by replacing the trailing 6 X's on the user's
-   input string with process id and a unique letter. it is assumed but not
-   checked that the X's are present. this function has a classic race in it,
-   which is exacerbated by the fact that tahoe pids are not unique in their
-   lower 5 digits. we attempt some mitigation with the access() outer loop */
+   input string with a set of characters to based on the current process ID
+   and a sequential counter, which are both encoded using a form of base 62.
+   this function suffers from a well-known race condition which has security
+   implications; the problem is rooted in interface, not implementation, so
+   it can't be addressed here. (that's fine because we don't really care.)
+
+   n.b. we implement the ANSI function tmpnam() by wrapping mktemp(), thus
+   TMP_MAX (in stdio.h) is somewhat dependent on BASE. keep them in sync. */
+
+#define NR_XS       6       /* number of Xs expected in template */
+#define BASE        62      /* the base of the encoded numbers */
 
 char *mktemp(char *template)
 {
-    static char generator = 'a';
+    static const char encode[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"   /* 26 + */
+                                 "abcdefghijklmnopqrstuvwxyz"   /* 26 + */
+                                 "1234567890";                  /* 10 = 62 */
+
+    /* we remember the last sequence number used. this is a simple way
+       to limit the number of access() calls made when a process makes
+       several temp files from the same template, a common occurrence */
+
+    static const char *next = encode;
 
     char *p;
     int i, pid;
 
     pid = getpid();
 
-    do {
-        for (p = template; *p; p++)
-            ;
+    for (p = template; *p; ++p) ;           /* NUL terminator -> p */
 
-        switch (generator)  {
-        case '~':
-            *--p = generator;
-            generator = 'A';
-            break;
-        case '_':
-            *--p = generator;
-            generator = '!';
-            break;
-        case '*':
-            *--p = ++generator;
-            generator++;
-            break;
-        case '?':
-            *--p = (generator = '@');
-            break;
-        case '@':
-            generator = 'a';
-            *--p = generator++;
-            break;
-        default:
-            *--p = generator++;
-        }
+    if ((p - template) < NR_XS)             /* too short? don't go */
+        goto einval;                        /* writing into random memory */
 
-        for (i = 0; i < 5; ++i) {
-            *--p = pid % 10 + '0';
-            pid /= 10;
-        }
-    } while (access(template, F_OK) >= 0);
+    for (i = 0; i < NR_XS; ++i)             /* check for trailing Xs. not */
+        if (*--p != 'X') goto einval;       /* required by POSIX, but ... */
 
-    return (template);
+    for (i = 0; i < (NR_XS - 1); ++i) {     /* replace first Xs with pid, */
+        *p++ = encode[pid % BASE];          /* base-62 encoded (backwards) */
+        pid /= BASE;
+    }
+
+    for (i = 0; i < BASE; ++i) {
+        *p = *next++;
+        if (*next == 0) next = encode;
+
+        /* we assume an error means "does not exist", though it
+           could be any number of other things. the onus is on
+           the caller to check the return code of the subsequent
+           attempt to open the file (as it should be, of course) */
+
+        if (access(template, F_OK) < 0) goto success;
+    }
+
+    /* can't find an unused name. POSIX
+       does not specify an errno here. */
+
+    goto toomany;
+
+einval:
+    errno = EINVAL;
+toomany:
+    *template = 0;
+success:
+    return template;
 }
 
 /* vi: set ts=4 expandtab: */

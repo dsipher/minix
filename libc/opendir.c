17d6dd7cf02a4fd04bf7e3c07a8346434f296835
Charles E. Youse <charles@gnuless.org>
2022-10-24 08:55:36 -0400

    libc: add missing #include directives
    discovered a few implicit declarations when compiling
    with warnings on, so added the appropriate #includes.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- libc/opendir.c
+++ libc/opendir.c
@@ -40,6 +40,7 @@
 *****************************************************************************/
 
 #include <dirent.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>

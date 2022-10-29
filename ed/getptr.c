c07237e45bf2e6334b7ba394c6548cf02ba09251
Charles E. Youse <charles@gnuless.org>
2022-10-29 15:29:34 -0400

    ed: separate out getptr.c
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

---
+++ ed/getptr.c
@@ -0,0 +1,48 @@
+/*****************************************************************************
+
+   getptr.c                                              ux/64 line editor
+
+******************************************************************************
+
+    derived from source found in Minix 2, Copyright 1987 Brian Beattie.
+
+              Permission to copy and/or distribute granted
+              under the following conditions:
+
+                 (1) No charge may be made other than
+                     reasonable charges for reproduction.
+
+                 (2) This notice must remain intact.
+                 (3) No further restrictions may be added.
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include "tools.h"
+#include "ed.h"
+
+LINE *getptr(int num)
+{
+    LINE *ptr;
+    int j;
+
+    if (2 * num > lastln && num <= lastln) {
+        /* high line numbers */
+
+        ptr = line0.l_prev;
+
+        for (j = lastln; j > num; j--)
+            ptr = ptr->l_prev;
+    } else {
+        /* low line numbers */
+
+        ptr = &line0;
+
+        for (j = 0; j < num; j++)
+            ptr = ptr->l_next;
+    }
+
+    return (ptr);
+}
+
+/* vi: set ts=4 expandtab: */

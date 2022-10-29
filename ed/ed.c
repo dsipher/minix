063f162e6bfff9e3441467812bb1e137193639c8
Charles E. Youse <charles@gnuless.org>
2022-10-29 18:27:12 -0400

    ed: clean up ed.c
    only whitespace/formatting changes.
    still builds precisely the same binary.
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

--- ed/ed.c
+++ ed/ed.c
@@ -17,21 +17,6 @@
 
 *****************************************************************************/
 
-/*	This program used to be in many little pieces, with this makefile:
-.SUFFIXES:	.c .s
-
-CFLAGS = -F
-
-OBJS =	append.s catsub.s ckglob.s deflt.s del.s docmd.s doglob.s\
-  doprnt.s doread.s dowrite.s ed.s egets.s find.s getfn.s getlst.s\
-  getnum.s getone.s getptr.s getrhs.s gettxt.s ins.s join.s maksub.s\
-  move.s optpat.s set.s setbuf.s subst.s getpat.s matchs.s amatch.s\
-  unmkpat.s omatch.s makepat.s bitmap.s dodash.s esc.s System.s
-
-ed:	$(OBJS)
-  cc -T. -i -o ed $(OBJS)
-*/
-
 #include <stdio.h>
 #include <setjmp.h>
 #include <signal.h>
@@ -43,96 +28,105 @@ ed:	$(OBJS)
 #include "tools.h"
 #include "ed.h"
 
-jmp_buf env;
-
-LINE line0;
-int curln = 0;
-int lastln = 0;
-char *inptr;
+jmp_buf     env;
+LINE        line0;
+int         curln = 0;
+int         lastln = 0;
+char        *inptr;
 static char inlin[MAXLINE];
-int nflg, lflg;
-int line1, line2, nlines;
+int         nflg, lflg;
+int         line1, line2, nlines;
 extern char fname[];
-int version = 1;
-int diag = 1;
+int         version = 1;
+int         diag = 1;
 
-void intr(sig)
-int sig;
+void intr(int sig)
 {
-  printf("?\n");
-  longjmp(env, 1);
+    printf("?\n");
+    longjmp(env, 1);
 }
 
-int main(argc, argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
-  int stat, i, doflush;
+    int stat, i, doflush;
 
-  set_buf();
-  doflush = isatty(1);
+    set_buf();
+    doflush = isatty(1);
 
-  if (argc > 1 && (strcmp(argv[1], "-") == 0 || strcmp(argv[1], "-s") == 0)) {
-	diag = 0;
-	argc--;
-	argv++;
-  }
-  if (argc > 1) {
-	for (i = 1; i < argc; i++) {
-		if (doread(0, argv[i]) == 0) {
-			curln = 1;
-			strcpy(fname, argv[i]);
-			break;
-		}
-	}
-  }
-  while (1) {
-	setjmp(env);
-	if (signal(SIGINT, SIG_IGN) != SIG_IGN) signal(SIGINT, intr);
+    if (argc > 1 && (strcmp(argv[1], "-") == 0
+                 || strcmp(argv[1], "-s") == 0))
+    {
+        diag = 0;
+        argc--;
+        argv++;
+    }
 
-	if (doflush) fflush(stdout);
+    if (argc > 1) {
+        for (i = 1; i < argc; i++) {
+            if (doread(0, argv[i]) == 0) {
+                curln = 1;
+                strcpy(fname, argv[i]);
+                break;
+            }
+        }
+    }
 
-	if (fgets(inlin, sizeof(inlin), stdin) == NULL) {
-		break;
-	}
-	for (;;) {
-		inptr = strchr(inlin, EOS);
-		if (inptr >= inlin+2 && inptr[-2] == '\\' && inptr[-1] == NL) {
-			inptr[-1] = 'n';
-			if (fgets(inptr, sizeof(inlin) - (inptr - inlin),
-						stdin) == NULL) break;
-		} else {
-			break;
-		}
-	}
-	if (*inlin == '!') {
-		if ((inptr = strchr(inlin, NL)) != NULL) *inptr = EOS;
-		sys(inlin + 1);
-		continue;
-	}
-	inptr = inlin;
-	if (getlst() >= 0)
-		if ((stat = ckglob()) != 0) {
-			if (stat >= 0 && (stat = doglob()) >= 0) {
-				curln = stat;
-				continue;
-			}
-		} else {
-			if ((stat = docmd(0)) >= 0) {
-				if (stat == 1) doprnt(curln, curln);
-				continue;
-			}
-		}
-	if (stat == EOF) {
-		exit(0);
-	}
-	if (stat == FATAL) {
-		fputs("FATAL ERROR\n", stderr);
-		exit(1);
-	}
-	printf("?\n");
-  }
-  return(0);
+    while (1) {
+        setjmp(env);
+
+        if (signal(SIGINT, SIG_IGN) != SIG_IGN)
+            signal(SIGINT, intr);
+
+        if (doflush) fflush(stdout);
+
+        if (fgets(inlin, sizeof(inlin), stdin) == NULL)
+            break;
+
+        for (;;) {
+            inptr = strchr(inlin, EOS);
+
+            if (inptr >= inlin+2 && inptr[-2] == '\\' && inptr[-1] == NL) {
+                inptr[-1] = 'n';
+
+                if (fgets(inptr, sizeof(inlin) - (inptr - inlin), stdin) == 0)
+                    break;
+            } else
+                break;
+        }
+
+        if (*inlin == '!') {
+            if ((inptr = strchr(inlin, NL)) != NULL) *inptr = EOS;
+            sys(inlin + 1);
+            continue;
+        }
+
+        inptr = inlin;
+
+        if (getlst() >= 0)
+            if ((stat = ckglob()) != 0) {
+                if (stat >= 0 && (stat = doglob()) >= 0) {
+                    curln = stat;
+                    continue;
+                }
+            } else {
+                if ((stat = docmd(0)) >= 0) {
+                    if (stat == 1) doprnt(curln, curln);
+                    continue;
+                }
+            }
+
+        if (stat == EOF)
+            exit(0);
+
+        if (stat == FATAL) {
+            fputs("FATAL ERROR\n", stderr);
+            exit(1);
+        }
+
+        printf("?\n");
+    }
+
+    return 0;
 }
 
 /* vi: set ts=4 expandtab: */

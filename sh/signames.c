b8cc62c7526d7caff98c823c8a45bc9e0a22339b
Charles E. Youse <charles@gnuless.org>
2022-10-30 10:42:49 -0400

    sh: eliminate mksignames.c
    use pre-built signames.c and signames.h.
    
    these should really themselves be eliminated in favor
    of a signal list in the standard library, something
    along the lines of strerror() or __sys_errlist[].
    
    Signed-off-by: Charles E. Youse <charles@gnuless.org>

---
+++ sh/signames.c
@@ -0,0 +1,56 @@
+/*****************************************************************************
+
+   signames.c                                                  ux/64 shell
+
+******************************************************************************
+
+   derived from ash, contributed to Berkeley by Kenneth Almquist.
+   Copyright (c) 1991 The Regents of the University of California.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS  PROVIDED BY  THE COPYRIGHT  HOLDERS AND  CONTRIBUTORS
+   "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES,  INCLUDING, BUT  NOT
+   LIMITED TO, THE  IMPLIED  WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS
+   FOR  A  PARTICULAR  PURPOSE  ARE  DISCLAIMED.  IN  NO  EVENT  SHALL THE
+   COPYRIGHT  HOLDER OR  CONTRIBUTORS BE  LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL,  SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL  DAMAGES (INCLUDING,
+   BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
+   OF USE, DATA, OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY  OF LIABILITY, WHETHER IN CONTRACT,  STRICT LIABILITY, OR
+   TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY OUT OF THE
+   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*****************************************************************************/
+
+#include "shell.h"
+
+char *const sigmesg[16] = {
+      0,
+      "Hangup",
+      "Interrupt",
+      "Quit",
+      "Illegal instruction",
+      "Trace/BPT trap",
+      "abort",
+      "Bus error",
+      "Floating exception",
+      "Killed",
+      0,
+      "Memory fault",
+      0,
+      "Broken pipe",
+      "Alarm call",
+      "Terminated",
+};
+
+/* vi: set ts=4 expandtab: */
